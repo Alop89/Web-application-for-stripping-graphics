1129be56bb80870bba8a24e2f36260ef8224461a
Alop89 <ibt.jlopezc@gmail.com>
2024-03-05 20:07:34 -0600

    Cambio de línea donde está dataset

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-car_data = pd.read_csv('/home/alfredo/Documentos/Tripleten_da/Project_5/project_5/vehicles_us.csv')
+car_data = pd.read_csv('/project_5/vehicles_us.csv')
 
 st.header('Gráfico de industria automotriz')
 
